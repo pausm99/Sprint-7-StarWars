724535a65b6c336ca48ba8c6ed788bec59b0fda4
pausm99 <pausm99@gmail.com>
2023-11-23 11:56:13 +0100

    Exercici 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { TopNavbarComponent } from './components/shared/top-navbar/top-navbar.component';
 
 @Component({
